c6c43182d672e45371fcfb239ff280d51c85bb01
Stacey-Lee-hub <staceypietersen2@gmail.com>
2026-01-08 16:21:24 +0200

    fixed movement lag and started with terrain

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed = 7;
+    public float speed = 6;
+    public int facingDirection = 1;
+
     public Rigidbody2D rb;
     public Animator anim;
 
@@ -11,9 +13,21 @@ public class PlayerMovement : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        anim.SetFloat("horizontal", horizontal);
-        anim.SetFloat("vertical", vertical);
+        // Flips when going backwards
+        if(horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0) {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
 
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip() {
+        // opposite value of itself
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
